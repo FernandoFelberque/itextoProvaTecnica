83c354b6abd4b61927035e7cb5b0be8f01e92237
Fernando <fernandocaiofelberque@gmail.com>
2024-10-04 16:48:39 -0300

    report

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,8 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://devall.com.br',
-    setupNodeEvents(on, config) {},
+    setupNodeEvents(on, config) {
+      require('cypress-html-reporter/GenerateReport')(on, config)
+    },
   },
 });
